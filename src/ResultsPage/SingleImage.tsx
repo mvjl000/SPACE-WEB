5d2c9d1a62960799c26095911b2fb4e52363fd9f
mvjl000 <milosz.devmail@gmail.com>
2021-01-25 11:15:47 +0100

    Add view title feature for mobile

--- src/ResultsPage/SingleImage.tsx
+++ src/ResultsPage/SingleImage.tsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { FetchedImage } from "../types/fetchedImages";
 
 const SingleImage: React.FC<FetchedImage> = ({ links, data }) => {
+  const [showTite, setShowTitle] = useState(false);
+
   return (
     <div className="results__imageContainer" key={links[0].href}>
-      <div className="results__imageCover">
+      <div
+        onClick={() => setShowTitle(!showTite)}
+        className={`results__imageCover${
+          showTite ? " results__imageCover--active" : ""
+        }`}
+      >
         <p>{data[0].title}</p>
       </div>
-      <img src={links[0].href} alt={links[0].href} />
+      <img src={links[0].href} alt={links[0].href} loading="lazy" />
     </div>
   );
 };
