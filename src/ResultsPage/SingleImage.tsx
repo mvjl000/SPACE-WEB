ebb9717de9377ca886643c21e4ec29798590dd4f
mvjl000 <milosz.devmail@gmail.com>
2021-01-25 02:28:19 +0100

    Add image hover effect

--- src/ResultsPage/SingleImage.tsx
+++ src/ResultsPage/SingleImage.tsx
@@ -4,6 +4,9 @@ import { FetchedImage } from "../types/fetchedImages";
 const SingleImage: React.FC<FetchedImage> = ({ links, data }) => {
   return (
     <div className="results__imageContainer" key={links[0].href}>
+      <div className="results__imageCover">
+        <p>{data[0].title}</p>
+      </div>
       <img src={links[0].href} alt={links[0].href} />
     </div>
   );
