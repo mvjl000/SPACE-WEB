35d622f0cfe03d6b5f21984b147851e50c3caef6
mvjl000 <milosz.devmail@gmail.com>
2021-01-26 11:49:01 +0100

    Add proper error hanling

--- src/Results/ResultsPage.tsx
+++ src/Results/ResultsPage.tsx
@@ -19,9 +19,10 @@ const ResultsPage = () => {
     { data: [{ title: "" }], links: [{ href: "" }] },
   ]);
   const [isLoading, setIsLoading] = useState(true);
+  const [isFetchingError, setIsFetchingError] = useState(false);
   const { phrase } = useParams<ResultsParams>();
 
-  useEffect(() => {
+  useEffect((): void => {
     const fetchData = async () => {
       try {
         setIsLoading(true);
@@ -31,7 +32,8 @@ const ResultsPage = () => {
         setIsLoading(false);
         setImages(response.data.collection.items);
       } catch (err) {
-        console.log(err);
+        setIsLoading(false);
+        setIsFetchingError(true);
       }
     };
     fetchData();
@@ -66,25 +68,39 @@ const ResultsPage = () => {
           animate={{ opacity: 1 }}
           transition={{ duration: 1, delay: 0.7 }}
         >
-          {images.length > 1 ? (
-            images.map(({ links, data }, index) => {
-              if (index < 28)
-                return (
-                  <SingleImage key={links[0].href} links={links} data={data} />
-                );
-              return undefined;
-            })
-          ) : (
-            <motion.h2
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ duration: 0.5, delay: 0.6 }}
-              className="errorMessage"
-            >
-              No images found, please try other phrase.
-            </motion.h2>
-          )}
+          {images.length > 1
+            ? images.map(({ links, data }, index) => {
+                if (index < 28)
+                  return (
+                    <SingleImage
+                      key={links[0].href}
+                      links={links}
+                      data={data}
+                    />
+                  );
+                return undefined;
+              })
+            : !isFetchingError && (
+                <motion.h2
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  transition={{ duration: 0.5, delay: 0.6 }}
+                  className="errorMessage"
+                >
+                  No images found, please try other phrase.
+                </motion.h2>
+              )}
         </motion.div>
+      )}
+      {!isLoading && isFetchingError && (
+        <motion.h2
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5, delay: 0.6 }}
+          className="errorMessage"
+        >
+          Some error ocurred during fetching data. Please try again later
+        </motion.h2>
       )}
     </div>
   );
